4ff580a63774f91b706749c8425883c6dc4ed61f
karine-engineer <karinemkrtcan5880@gmail.com>
2024-11-25 22:53:51 +0700

    Add files via upload

---
+++ 2024.11.10/5.py
@@ -0,0 +1,22 @@
+from math import prod
+def central_tendency(a: float, b: float, *nums: float) -> dict[str, float]:
+    total={}
+    numbers=[a,b]+list(nums)
+    s_nums=sorted(numbers)
+    n=len(s_nums)
+    if n%2!=1:
+        middle_index=n//2
+        total['median']=(s_nums[middle_index-1]+s_nums[middle_index])/2
+    else:
+        total['median']=s_nums[n//2]
+    total['arithmetic']=sum(s_nums)/n
+    total['geometric']=prod(numbers)**(1/n)
+    total['harmonic']=n/ sum(1 / num for num in s_nums)
+    return total
+    
+#>>> central_tendency(1, 2, 3, 4)
+#{'median': 2.5, 'arithmetic': 2.5, 'geometric': 2.2133638394006434, 'harmonic': 1.92}
+    
+#>>> sample = [1, 2, 3, 4, 5] 
+#>>> central_tendency(*sample)
+#{'median': 3, 'arithmetic': 3.0, 'geometric': 2.605171084697352, 'harmonic': 2.18978102189781}
---
+++ 2024.11.10/6.py
@@ -0,0 +1,23 @@
+from math import sqrt
+def orth_triangle(cathetus1: float = 0, cathetus2: float = 0, hypotenuse: float = 0) -> float | None:
+    sides = [cathetus1, cathetus2, hypotenuse]
+   
+    if sides.count(0) != 1:
+        return None
+    
+    if hypotenuse > 0 and (cathetus1 <= 0 and cathetus2 <= 0 or (cathetus1 > hypotenuse or cathetus2 > hypotenuse)):
+        return None
+    if hypotenuse > 0:
+        if cathetus1 > 0:
+            return sqrt(hypotenuse**2 - cathetus1**2)
+        else:
+            return sqrt(hypotenuse**2 - cathetus2**2)
+    else:
+        return sqrt(cathetus1**2 + cathetus2**2)
+
+#>>> orth_triangle(cathetus1=3, hypotenuse=5)
+#4.0 
+#>>> orth_triangle(cathetus1=8, cathetus2=15)
+#17.0 
+#>>> orth_triangle(cathetus2=9, hypotenuse=3)
+#None
