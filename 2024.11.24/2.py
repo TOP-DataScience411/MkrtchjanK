97cdbf4fd5f1db6b676b95e25d5e64ec79e3a6b2
karine-engineer <karinemkrtcan5880@gmail.com>
2024-12-01 16:02:12 +0700

    Add files via upload

---
+++ 2024.11.24/2.py
@@ -0,0 +1,14 @@
+def product(nums):
+    if not nums:
+        return 1.0
+    first, *rest=nums
+    total=float(first)*product(rest)
+    if total==0:
+        return 0.0
+    else:
+        return total
+    
+#>>> product(range(10, 60, 10))
+#12000000.0
+#>>> product((0.12, 0.05, -0.09, 0.0, 0.21))
+#0.0
