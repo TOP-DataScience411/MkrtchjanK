674ba23bf80a23e5c7f4b9a8186b1d9c6f144c99
karine-engineer <karinemkrtcan5880@gmail.com>
2024-12-01 15:18:52 +0700

    Delete 2024.11.24/1.py

--- 2024.11.24/1.py
+++
@@ -1,9 +0,0 @@
-def deck():
-    suits = ['черви', 'бубны', 'пики', 'трефы']
-    values=list(range(2,11))+['валет', 'дама', 'король','туз']
-    for suit in suits:
-        for value in values: 
-            yield (value, suit)
-
-print(list(deck())[::13])
-#[(2, 'черви'), (2, 'бубны'), (2, 'пики'), (2, 'трефы')]
